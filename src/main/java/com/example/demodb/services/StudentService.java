9a5179261ee6e3334436fc7684d4c0b505873e42
Anvar78 <bulbuliam17@gmail.com>
2024-04-09 13:32:15 +0530

    Second

--- src/main/java/com/example/demodb/services/StudentService.java
+++ src/main/java/com/example/demodb/services/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demodb.services;
 import com.example.demodb.models.Student;
 import com.example.demodb.repos.StudentRepos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +13,15 @@ public class StudentService {
 
     public void addStudent(Student s1){
        studentr.save(s1);
+    }
+
+    public Student updateStudent(int id, String city) {
+        Student s1= studentr.findById(id).get();
+        s1.setCity(city);
+        return studentr.save(s1);
+    }
+    public String deleteStudent(int id){
+      studentr.deleteById(id);
+      return "Student ID:"+id+" Deleted";
     }
 }
